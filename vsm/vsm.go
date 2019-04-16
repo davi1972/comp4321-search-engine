cd2c4fee3efe3861447999f88b38590092723282
Cheng Chee Hau Calvin <chccheng@connect.ust.hk>
2019-04-16 17:58:45 +0800

    Finished preliminary version of vector space model

--- vsm/vsm.go
+++ vsm/vsm.go
@@ -7,38 +7,32 @@ import (
 	"github.com/davi1972/comp4321-search-engine/tokenizer"
 )
 
-func stringToWordID(qterm string) uint64 {
+// Returns a wordid given a (tokenized) term.
+func StringToWordID(qterm string) uint64 {
 	wordsindex := &Indexer.MappingIndexer{}
 	wordid, _ := wordsindex.GetValueFromKey(qterm)
 	return wordid
 }
 
-// Returns the inverse document frequency
-func inverseDocumentFreq(qterm string, documentId uint64) float64 {
+// Returns the inverse document frequency of a string.
+func InverseDocumentFreq(qterm string) float64 {
 	// log_2(N/df of term)
 	N := 0
 	df := 0
+
 	pageindex := &Indexer.PagePropetiesIndexer{}
 	pages, _ := pageindex.All()
 
-	// get N
-	for i := range pages {
-		N++
-	}
-
-	// get no. of docs with qterm
 	invertedIndex := &Indexer.InvertedFileIndexer{}
-	invertedFile, _ := invertedIndex.GetInvertedFileFromKey(stringToWordID(qterm))
+	invertedFile, _ := invertedIndex.GetInvertedFileFromKey(StringToWordID(qterm))
 
-	for i := range invertedFile {
-		df++
-	}
-
+	N = len(pages)
+	df = len(invertedFile)
 	return math.Log2(float64(N) / float64(df))
 }
 
-// Returns the term frequency of a term in document
-func termFreq(qterm string, documentID uint64) uint64 {
+// Returns the term frequency of a term in document (ID).
+func TermFreq(qterm string, documentID uint64) uint64 {
 	// frequency of term j in document i
 	docwords := &Indexer.DocumentWordForwardIndexer{}
 	words, _ := docwords.GetWordFrequencyListFromKey(documentID)
@@ -54,19 +48,16 @@ func termFreq(qterm string, documentID uint64) uint64 {
 	return 0
 }
 
-func computeTermWeight(qterm string, documentID uint64) float64 {
-	maxtf := maxTermFreq(qterm, documentID)
-	return float64(termFreq(qterm, documentID)/maxtf) * inverseDocumentFreq(qterm, documentID)
+// Returns the computed term weight of a (tokenized) term given a string and document (ID).
+func ComputeTermWeight(qterm string, documentID uint64) float64 {
+	return float64(TermFreq(qterm, documentID)/MaxTermFreq(documentID)) * InverseDocumentFreq(qterm)
 }
 
-func computeQueryWeight(qterm string) float64 {
-
-}
-
-func maxTermFreq(qterm string, documentID uint64) uint64 {
+// Returns the maximum term frequency of a term in a document ID.
+func MaxTermFreq(documentID uint64) uint64 {
 	docwords := &Indexer.DocumentWordForwardIndexer{}
 	words, _ := docwords.GetWordFrequencyListFromKey(documentID)
-	wordID := stringToWordID(qterm)
+	//wordID := stringToWordID(qterm)
 
 	wf := words[0]
 	for i := range words[1:] {
@@ -78,19 +69,27 @@ func maxTermFreq(qterm string, documentID uint64) uint64 {
 	return wf.GetFrequency()
 }
 
-// Returns the cosine similarity between query and document ID
-func cosSimilarity(query string, documentID uint64) float64 {
-	// use doc id to find terms
-	// forea(dw * qw) / forea(doclen) * forea(qlen)
-	similarity := 0.0
+// Returns the cosine similarity between query and document ID.
+func CosSimilarity(query string, documentID uint64) float64 {
 	terms := tokenizer.Tokenize(query)
-	docwords := &Indexer.DocumentWordForwardIndexer{}
-	words, _ := docwords.GetWordFrequencyListFromKey(documentID)
+	termWeights := make(map[string]float64)
+	queryFreq := make(map[string]int)
 
-	// dik is weight of term k in doc i, qk is weight of term k in query
-	var innerPro float64 = 0.0
 	for i := range terms {
-		innerPro += computeTermWeight(terms[i], documentID) + computeQueryWeight(terms[i])
+		termWeights[terms[i]] = ComputeTermWeight(terms[i], documentID)
+		queryFreq[terms[i]]++
+	}
+	// dik is weight of term k in doc i, qk is weight of term k in query
+	innerPro := 0.0
+	sumD := 0.0
+	sumQ := 0.0
+
+	for i := 0; i < len(queryFreq); i++ {
+		invDocFreq := InverseDocumentFreq(terms[i])
+		innerPro += termWeights[terms[i]] * (float64(queryFreq[terms[i]]) * invDocFreq)
+		sumD += termWeights[terms[i]] * termWeights[terms[i]]
+		sumQ += (float64(queryFreq[terms[i]]) * invDocFreq) * (float64(queryFreq[terms[i]]) * invDocFreq)
 	}
 
+	return innerPro / (math.Sqrt(sumD) * math.Sqrt(sumQ))
 }
